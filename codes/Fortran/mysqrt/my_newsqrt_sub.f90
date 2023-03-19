57e031f05ab3ef51bdd4bd720f507e831e31deed
Abhishek Sharma <d22027@students.iitmandi.ac.in>
2023-03-19 10:45:45 +0530

    my_sub is corrected

--- codes/Fortran/mysqrt/my_newsqrt_sub.f90
+++ codes/Fortran/mysqrt/my_newsqrt_sub.f90
@@ -4,56 +4,29 @@
 program mysqrt
 	
 	implicit none
-	integer::i
-	real (kind=8):: y,x
+	integer::i,kmax
+	real (kind=8):: y,x,tol
+	tol=1.d0-14
+	kmax=1000
 	print *, "Please enter the positive number"
 	read *, y
-	call mysqroot(y,x,i)
+	call mysqroot(y,kmax,tol,x,i)
 	print*,"The root of ",y," is = ", x, "at the iteraton ",i
 
 end program
 
-subroutine mysqroot(y,x,i)
+subroutine mysqroot(y,kmax,tol,x,i)
 	implicit none
 	real(kind=8),intent(inout)::x
-	real(kind=8), intent(in)::y
+	real(kind=8), intent(in)::y,tol
+	integer,intent(in)::kmax
 	integer,intent(inout)::i
-	real (kind=8):: tol,tmp
-	tol=1.d0-14
-	x=1.d0 !initial guess
-	do i=1,1000
+	real (kind=8)::tmp
+	x=4.d0 !initial guess
+	do i=1,kmax
 		tmp=x
 		x=0.5d0*(x+y/x)
-		if(abs(tmp-x)/x.LT.tol) then
-			!print*, "Root is converged and root is",x,i
-			exit
-		end if! A fortran subroutine for calculating square root 
-! using the Newton's method
-
-program mysqrt
-	
-	implicit none
-	integer::i
-	real (kind=8):: y,x
-	print *, "Please enter the positive number"
-	read *, y
-	call mysqroot(y,x,i)
-	print*,"The root of ",y," is = ", x, "at the iteraton ",i
-
-end program
-
-subroutine mysqroot(y,x,i)
-	implicit none
-	real(kind=8),intent(inout)::x
-	real(kind=8), intent(in)::y
-	integer,intent(inout)::i
-	real (kind=8):: tol,tmp
-	tol=1.d0-14
-	x=1.d0 !initial guess
-	do i=1,1000
-		tmp=x
-		x=0.5d0*(x+y/x)
-		if(abs(tmp-x)/x.LT.tol) then
+		if(abs(tmp-x)/y.LT.tol) then
 			!print*, "Root is converged and root is",x,i
 			exit
 		end if
