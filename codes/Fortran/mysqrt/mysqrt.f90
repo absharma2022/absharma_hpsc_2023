62406d51d193c0340b2628d9fdf267e4063e6162
Abhishek Sharma <d22027@students.iitmandi.ac.in>
2023-03-19 10:04:07 +0530

    mysqrt subroutine is updated, no. of iteration also retruned with sqrt value

--- codes/Fortran/mysqrt/mysqrt.f90
+++ codes/Fortran/mysqrt/mysqrt.f90
@@ -4,27 +4,28 @@
 program mysqrt
 	
 	implicit none
+	integer::i
 	real (kind=8):: y,x
 	print *, "Please enter the positive number"
 	read *, y
-	call mysqroot(y,x)
-	print*, "The root of n is = ", x
+	call mysqroot(y,x,i)
+	print*,"The root of ",y," is = ", x, "at the iteraton ",i
 
 end program
 
-subroutine mysqroot(y,x)
+subroutine mysqroot(y,x,i)
 	implicit none
 	real(kind=8),intent(inout)::x
 	real(kind=8), intent(in)::y
-	integer :: i
+	integer,intent(inout)::i
 	real (kind=8):: tol,tmp
 	tol=1.d0-14
 	x=1.d0 !initial guess
 	do i=1,1000
 		tmp=x
 		x=0.5d0*(x+y/x)
-		if(abs(tmp-x).LT.tol) then
-			print*, "Root is converged and root is",x,i
+		if(abs(tmp-x)/x.LT.tol) then
+			!print*, "Root is converged and root is",x,i
 			exit
 		end if
 	end do
